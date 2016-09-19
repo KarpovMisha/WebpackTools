9512f9965018b69492783f0acc27d138de67edc8
Михаил Карпов < >
2016-09-19 15:40:11 +0300

    Inital commit

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import s from './App.scss';
+
+
+class App extends Component {
+
+    render() {
+        return (
+
+                <h1>Starter</h1>
+
+
+        );
+    }
+}
+
+export default App;
