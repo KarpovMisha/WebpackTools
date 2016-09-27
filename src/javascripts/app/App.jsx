1c189e4ed8d9aebac1ddf168e5b3c23593c92ae6
Михаил Карпов < >
2016-09-27 14:36:53 +0300

    working version

--- src/javascripts/app/App.jsx
+++ src/javascripts/app/App.jsx
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
+
 import {} from 'stylesheets/main.scss';
 
-// минификация продакшена js, css
-    // удаления папки dist (clean webpack plugin)
-//  webpack hot reloader
 
-class App extends Component {
+class App extends React.Component {
 
     render() {
         return (
-                <div className="row">
-                    <div className="col-lg-6"><h1>hello</h1></div>
-                    <div className="col-lg-6"><h2>World!!!</h2></div>
-                </div>
+            <div className="title">
+                <h1>{this.props.text}</h1>
+            </div>
         );
     }
 }
--- src/javascripts/client.js
+++ src/javascripts/client.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './app/App.jsx';
 
 
+ReactDOM.render(
+    <App text="Instagram photo"/>,
+    document.querySelector('.container')
+);
 
-let element = React.createElement(App, {});
-render(element, document.querySelector('.container'));
+
+
+//let element = React.createElement(App , {});
+//render(element, document.querySelector('.container'));
 
 //module.hot.dispose()
 if (module.hot) {
